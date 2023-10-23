b00130bc333280d889361050985940549bfd82bb
AalguienN <93289662+AalguienN@users.noreply.github.com>
2023-10-23 17:07:14 +0200

    GameState en LevelManager

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public GameManager.GameState gs = GameManager.GameState.level;
     public Spawner spawner;
     public GameObject canvas;
 
     // Start is called before the first frame update
     void Start()
     {
+        GameManager.Instance.gameState = gs;
         if (spawner == null) Debug.LogError("Falta el spawner");
         if (canvas == null) Debug.LogError("Falta el canvas");
         //GameManager.Instance.set
