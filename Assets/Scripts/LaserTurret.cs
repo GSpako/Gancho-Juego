94a2a57ec04e8e42e69f92093923b670b2ac87ed
Cabanero11 <cabanero1111@gmail.com>
2023-10-24 18:44:35 +0200

    Torreta V1
    Es funcional, pero mejorable

--- Assets/Scripts/LaserTurret.cs
+++ Assets/Scripts/LaserTurret.cs
@@ -3,74 +3,125 @@ using UnityEngine;
 
 public class LaserTurret : MonoBehaviour
 {
-    public Transform player;
-    public GameObject boca; // El punto de origen del láser
+    [Header("Referencias")]
+    private Player player;
+    public Transform boca; // El punto de origen del láser
 
     [Header("Variables")]
     public float detectionRadius = 10f;
     public float rotationSpeed = 5f;
     public float shootingCooldown = 4f;
-    public float chargeDuration = 2f;
     public float laserSpeed = 2f;
-    public Player playerScript;
-
-    [Header("Sonidos")]
-    public AudioClip greenLaserSound;
-    public AudioClip redLaserSound;
-    public AudioClip hitPlayerSound;
-    private AudioSource audioSource;
-
+    public float laserMaxLength = 20f;
+    public float playerPositionDelay = 1f;
+    public LayerMask playerLayer; 
     private bool playerDetected = false;
     private bool canShoot = true;
-    private WaitForSeconds chargeDurationWait;
-    private WaitForSeconds shootingCooldownWait;
-
     private LineRenderer laserLine;
-    private Material greenLaserMaterial;
-    private Material redLaserMaterial;
+
+    private Vector3 lastPlayerPosition; // Nueva variable para almacenar la última posición del jugador
 
     private void Start()
     {
-        greenLaserMaterial = new Material(Shader.Find("Standard"));
-        greenLaserMaterial.color = Color.green;
-
-        redLaserMaterial = new Material(Shader.Find("Standard"));
-        redLaserMaterial.color = Color.red;
-
-        chargeDurationWait = new WaitForSeconds(chargeDuration);
-        shootingCooldownWait = new WaitForSeconds(shootingCooldown);
-
-        if (Player.instance == null)
-        {
-            player = GameObject.FindGameObjectWithTag("Player").transform;
-        }
-
-        if (audioSource == null)
-        {
-            audioSource = gameObject.AddComponent<AudioSource>();
-        }
-
-        // Configurar el LineRenderer
         laserLine = gameObject.AddComponent<LineRenderer>();
-        ConfigureLineRenderer(laserLine, greenLaserMaterial);
-    }
+        ConfigureLineRenderer(laserLine, Color.red);
 
-    private void ConfigureLineRenderer(LineRenderer lineRenderer, Material material)
-    {
-        lineRenderer.startWidth = 0.1f;
-        lineRenderer.endWidth = 0.1f;
-        lineRenderer.material = material;
-        lineRenderer.enabled = false;
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        }
+
+        StartCoroutine(ScanForPlayer());
     }
 
     private void Update()
     {
-        DetectPlayer();
-        RotateTowardsPlayer();
-
-        if (playerDetected && canShoot)
+        if (playerDetected)
         {
-            StartCoroutine(ChargeAndShoot());
+            RotateTowardsPlayer();
+        }
+    }
+
+    private void ConfigureLineRenderer(LineRenderer lineRenderer, Color color)
+    {
+        lineRenderer.startWidth = 0.5f;
+        lineRenderer.endWidth = 0.5f;
+        lineRenderer.material = new Material(Shader.Find("Standard"));
+        lineRenderer.material.color = color;
+        lineRenderer.enabled = false;
+    }
+
+    private IEnumerator ScanForPlayer()
+    {
+        while (true)
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, detectionRadius);
+
+            foreach (var collider in colliders)
+            {
+                if (collider.CompareTag("Player"))
+                {
+                    lastPlayerPosition = collider.transform.position;
+                    playerDetected = true;
+
+                    // Delay para que no sea la torreta destructora de players 3000
+                    yield return new WaitForSeconds(playerPositionDelay); 
+                    yield return StartCoroutine(ShootLaser());
+
+                    playerDetected = false;
+                    break; 
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator ShootLaser()
+    {
+        if (canShoot)
+        {
+            canShoot = false;
+            laserLine.enabled = true;
+
+            Vector3 directionToPlayer = lastPlayerPosition - boca.position; // Utilizar la última posición conocida
+            directionToPlayer.Normalize();
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(boca.position, directionToPlayer, out hit, laserMaxLength, playerLayer))
+            {
+                //Debug.Log("ahh disparie");
+                laserLine.SetPosition(0, boca.position);
+                laserLine.SetPosition(1, hit.point);
+
+                if (hit.collider.gameObject.CompareTag("Player"))
+                {
+                    //Debug.Log("ahh diablo me dio");
+                    Player.instance.kill();
+                }
+            }
+            else
+            {
+                laserLine.SetPosition(0, boca.position);
+                laserLine.SetPosition(1, boca.position + directionToPlayer * laserMaxLength);
+            }
+
+            yield return null;
+
+            laserLine.enabled = false;
+            yield return new WaitForSeconds(shootingCooldown);
+            canShoot = true;
+        }
+    }
+
+    private void RotateTowardsPlayer()
+    {
+        if (Player.instance != null)
+        {
+            Vector3 directionToPlayer = Player.instance.transform.position - transform.position;
+            Quaternion rotationToPlayer = Quaternion.LookRotation(directionToPlayer);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotationToPlayer, rotationSpeed * Time.deltaTime);
         }
     }
 
@@ -78,76 +129,5 @@ public class LaserTurret : MonoBehaviour
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, detectionRadius);
-    }
-
-    private void DetectPlayer()
-    {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, detectionRadius);
-
-        foreach (var collider in colliders)
-        {
-            if (collider.CompareTag("Player"))
-            {
-                player = collider.transform;
-                playerDetected = true;
-                return;
-            }
-        }
-
-        playerDetected = false;
-    }
-
-    private void RotateTowardsPlayer()
-    {
-        if (playerDetected)
-        {
-            transform.LookAt(player.position);
-        }
-    }
-
-    private IEnumerator ChargeAndShoot()
-    {
-        canShoot = false;
-
-        // Cargar láser verde
-        laserLine.enabled = true;
-        laserLine.material = greenLaserMaterial;
-        audioSource.PlayOneShot(greenLaserSound);
-        yield return chargeDurationWait;
-        laserLine.enabled = false;
-
-        // Disparar láser rojo
-        laserLine.enabled = true;
-        laserLine.material = redLaserMaterial;
-
-        Vector3 laserDirection = player.position - boca.transform.position;
-        laserDirection.Normalize();
-
-        float elapsedTime = 0f;
-
-        while (elapsedTime < shootingCooldown)
-        {
-            laserLine.SetPosition(0, boca.transform.position);
-            laserLine.SetPosition(1, laserLine.GetPosition(1) + laserDirection * laserSpeed * Time.deltaTime);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        laserLine.enabled = false;
-        audioSource.PlayOneShot(redLaserSound);
-
-        // Verificar si el láser rojo colisiona con el jugador
-        RaycastHit hit;
-        if (Physics.Raycast(boca.transform.position, laserDirection, out hit, shootingCooldown))
-        {
-            if (hit.collider.CompareTag("Player"))
-            {
-                audioSource.PlayOneShot(hitPlayerSound);
-                playerScript.kill();
-            }
-        }
-
-        yield return shootingCooldownWait;
-        canShoot = true;
     }
 }
