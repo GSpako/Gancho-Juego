e160be0c6425a198c97fb0669a2bbe4ac5296f58
GSpako <116070219+GSpako@users.noreply.github.com>
2023-11-20 14:38:49 +0100

    COSIS

---
+++ Assets/Respawn.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            GameManager.Instance.LevelManager.spawner.Spawn(Spawner.types.player);
+        }
+    }
+}
--- Assets/Scripts/Trampas/Oscilator.cs
+++ Assets/Scripts/Trampas/Oscilator.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Oscilator : MonoBehaviour
 {
+    [SerializeField] float startDelay = 1;
     [SerializeField] float angle = 45;
     [SerializeField] float speed = 1;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,7 @@ public class Oscilator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localRotation = Quaternion.Euler(0f, 0f, angle * Mathf.Sin(Time.time * speed));
+        if(Time.time > startDelay)
+        transform.localRotation = Quaternion.Euler(0f, 0f, angle * Mathf.Sin((Time.time - startDelay) * speed));
     }
 }
