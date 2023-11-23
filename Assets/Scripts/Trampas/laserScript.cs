23e99a785b405060b60d5f2c5dff736ed2bf44fc
GSpako <116070219+GSpako@users.noreply.github.com>
2023-11-23 16:41:23 +0100

    laser arreglo1

--- Assets/Scripts/Trampas/laserScript.cs
+++ Assets/Scripts/Trampas/laserScript.cs
@@ -3,17 +3,24 @@ using System.Collections;
 
 public class laserScript : MonoBehaviour {
 	public float radius = 0.5f;
+	public int vertices = 3;
 	public Transform startPoint;
 	public Transform endPoint;
 	LineRenderer laserLine;
 	// Use this for initialization
 	void Start () {
 		laserLine = GetComponentInChildren<LineRenderer> ();
+
 		laserLine.SetWidth (radius, radius);
+		laserLine.numCornerVertices = vertices;
+		laserLine.numCapVertices = vertices;
+
+		laserLine.SetPosition(0, startPoint.position);
+		laserLine.SetPosition(1, endPoint.position);
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
 		laserLine.SetPosition (0, startPoint.position);
 		laserLine.SetPosition (1, endPoint.position);
 
