f0595783b618dad65783838ef134c5094779f08c
AalguienN <93289662+AalguienN@users.noreply.github.com>
2023-11-23 19:23:23 +0100

    Actualizado ventilador

--- Assets/Scripts/Trampas/Ventilador.cs
+++ Assets/Scripts/Trampas/Ventilador.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ventilador : MonoBehaviour
 {
     [SerializeField] float force;
+    [SerializeField] Transform aspasRotator;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,7 @@ public class Ventilador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        aspasRotator.Rotate(0,Time.deltaTime * 50 * force,0);
     }
 
     private void OnTriggerStay(Collider other)
