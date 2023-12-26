63174c5504e64ea886c9260ebcd23f37f9493585
Cabanero11 <cabanero1111@gmail.com>
2023-12-26 12:56:34 +0100

    Nivel 1 mio + Adrian tunel + Jugar sale Animatica

--- Assets/Scripts/Menus/MainMenuScript.cs
+++ Assets/Scripts/Menus/MainMenuScript.cs
@@ -12,9 +12,12 @@ public class MainMenuScript : MonoBehaviour
         // DESCOMENTAR ESTA ES LA WENA LA DE ABAJO LA USE PARA PROBAR SI PASA DE NIVEL QUE DA ERROR EESTA AHORA
         //Hola soy Adrián, lo he encontrado, si da error es porque no está añadida la escena en las build settings! 
         //Si da problemas revisar o me envias un mensaje
-         SceneManager.LoadScene(GameManager.Instance.levels[0]/*SceneManager.GetActiveScene().buildIndex + 1*/);
+        //SceneManager.LoadScene(GameManager.Instance.levels[0]/*SceneManager.GetActiveScene().buildIndex + 1*/);
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        GameManager.Instance.musicPlayedForCurrentLevel = false; // cambiamos de nivel, y ya puede haber nueva musica
+        //GameManager.Instance.musicPlayedForCurrentLevel = false; // cambiamos de nivel, y ya puede haber nueva musica
+
+
+        SceneManager.LoadScene("Animatica");
     }
 
     public void QuitGame()
